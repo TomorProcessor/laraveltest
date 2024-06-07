f0628bbf3711b36344d65f15a1807599c6562e95
Peter Tomordi <tomordi.peter@zengo.eu>
2024-06-07 09:21:24 +0200

    2nd

---
+++ resources/views/components/asdval.blade.php
@@ -0,0 +1,13 @@
+<div>
+    {{ $testContent = "<p>This is a test paragraph</p>"  }}
+    <h1>TEST</h1>
+    {{ $testContent  }} <!-- innerText -->
+    {!! $testContent !!} <!-- innerHtml -->
+
+    @foreach($array as $num)
+        <!-- $loop -> info a foeach-ről -->
+        This is a number
+        <b>{{ $num }}</b>
+        <br/>
+    @endforeach
+</div>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test_asd/{asdval}', function ($asdval) {
+    $uri = "test_asd/{$asdval}";
+
+    /**
+     * 5 másodpercig cache-el, cache lejáratkor hívja a függvényt,
+     * benne a use-al van megadva, hogy a kinti változó belül látszódjon
+     */
+    $post = cache()->remember($asdval, 5, function () use ($uri) {
+        print $uri;
+    });
+
+    //asdval nevű blade, $array változó átadva neki
+    return view('components.asdval', ['array' => [0, 1, 2, 3, 4, 5]]);
+})->where('asdval', '[A-z]');
